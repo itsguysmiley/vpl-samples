2711a055cc77f40251bef6981697c9150c411b6d
itsguysmiley <43255948+itsguysmiley@users.noreply.github.com>
2018-11-11 22:00:49 -0500

    Add files via upload

---
+++ python/child/helloworld.py
@@ -0,0 +1,3 @@
+# read a line from STDIN
+my_string = input()
+print(my_string)
---
+++ python/child/vpl_evaluate.sh
@@ -0,0 +1,6 @@
+python << "END_OF_PYTHON" > evaluate.cases
+print("case=Test 1")
+print("input=Hello, World!")
+print("output=Hello, World!")
+print("Remove me to make a positive test.")
+END_OF_PYTHON
